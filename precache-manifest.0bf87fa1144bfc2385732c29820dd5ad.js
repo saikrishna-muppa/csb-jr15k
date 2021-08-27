2252282853ba92345a7ad137d5557a55874e03be
Code Sandbox <hello@codesandbox.io>
2021-08-27 05:00:42 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.0bf87fa1144bfc2385732c29820dd5ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f3000f52947258da3f1f60c1d24d65",
+    "url": "https://saikrishna-muppa.github.io/csb-jr15k/index.html"
+  },
+  {
+    "revision": "ee3035fc72bb4610d2cb",
+    "url": "https://saikrishna-muppa.github.io/csb-jr15k/static/css/main.b6ecb997.chunk.css"
+  },
+  {
+    "revision": "b4e170e7cd2094df8bd2",
+    "url": "https://saikrishna-muppa.github.io/csb-jr15k/static/js/2.3c488020.chunk.js"
+  },
+  {
+    "revision": "ee3035fc72bb4610d2cb",
+    "url": "https://saikrishna-muppa.github.io/csb-jr15k/static/js/main.969a265d.chunk.js"
+  },
+  {
+    "revision": "495ae4a569f4d936a842",
+    "url": "https://saikrishna-muppa.github.io/csb-jr15k/static/js/runtime~main.52a30bfe.js"
+  }
+]);
